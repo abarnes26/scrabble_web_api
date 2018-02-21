a6cb9ba4fd96a14a00fe3222353cb180e9e577c0
abarnes26 <abarnes26@yahoo.com>
2018-02-21 11:42:40 -0700

    finall got the desired result. refactoring

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -6,38 +6,27 @@ class GameSerializer < ActiveModel::Serializer
   end
 
   def scores
-    # sorted = object.plays.sort_by do |play|
-    #            play.user_id
-    #          end
-    #
-    # user_1_id = sorted[0].user_id
-    # user_2_id = sorted[-1].user_id
-    #
-    # user_1_plays = Play.where(user_id: user_1_id)
-    # user_2_plays = Play.where(user_id: user_2_id)
-    #
-    # user_1_plays.map do |play|
-    #   value = {}
-    #     value["user_id"] = play.user_id
-    #     value["score"] = play.score
-    #     value
-    #
-    # binding.pry
+    sorted = object.plays.sort_by do |play|
+               play.user_id
+             end
 
-    array = object.plays.map do |play|
-      value = {}
-      value["user_id"] = play.user_id
-      value["score"] = play.score
-      value
-    end
+    user_1_id = sorted[0].user_id
+    user_2_id = sorted[-1].user_id
 
+    user_1_score = Play.where(user_id: user_1_id).sum("score")
+    user_2_score = Play.where(user_id: user_2_id).sum("score")
+
+    [assemble_player_score(user_1_id, user_1_score), assemble_player_score(user_2_id, user_2_score)]
     binding.pry
-
-    array.sum {|h| h["score"]}
   end
 
-  # def scores
-  #   turns.reduce{|sum, score| sum.merge( score ){|key, first_score, second_score| first_score + second_score}}
-  # end
+  def assemble_player_score(player, score)
+    {user_id: player, score: score}
+  end
+
+  def sorted
+  
+  end
+
 
 end
